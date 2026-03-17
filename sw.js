3756d53a4183a7163cb7c64c84cabda6f84ebbe4
padreava <72949554+padreava@users.noreply.github.com>
2026-03-17 20:44:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,10 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('fetch',e=>{});
+
+const CACHE="pmk-final";
+
+self.addEventListener("install",e=>{
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html","manifest.json"])));
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
